e8b1e8c9feefae750439129cf0428a165e650b00
anasachu2000 <muhammedanasea2000@gmail.com>
2024-07-17 00:31:50 +0530

    footer for mobile completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Navbar from "./components/navbar/Navbar";
 import Dashboard from "./pages/dashboard/Dashboard";
 import { ThemeProvider } from "./components/theme-provider";
-import { dshboardList } from "./components/sidebar/common";
-import { Link } from "react-router-dom";
+import MobileNavbarList from "./components/mobileNavbar/MobileNavbar";
 
 function App() {
   return (
@@ -13,27 +12,7 @@ function App() {
         <Sidebar />
         <Navbar />
         <Dashboard />
-        <div className="h-[56px] w-full border-[1px] border-t-[#0000001a] bg-[#ffff] sticky bottom-0 md:hidden">
-          <div className="mx-auto  px-6 3xl:px-0">
-            <ul className="flex  items-center justify-between overflow-x-scroll">
-              {dshboardList.map((item) => (
-                <li key={item.id} className="hover:bg-gray-200 rounded-sm">
-                  <Link
-                    to={item.link}
-                    className="flex flex-col gap-[10px] items-center p-2 dark:hover:bg-gray-700 group"
-                  >
-                    <div className="w-[34px] h-[34px] rounded-3xl bg-[#EFEDEA] flex items-center justify-center">
-                      {item.icon}
-                    </div>
-                    <span className="list_fonts flex-1 ml-3 whitespace-nowrap">
-                      {item.name}
-                    </span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <MobileNavbarList />
       </div>
     </ThemeProvider>
   );
--- src/components/mobileNavbar/common.tsx
+++ src/components/mobileNavbar/common.tsx
@@ -2,40 +2,40 @@ import { MdDashboard, MdOutlineCurrencyRupee } from "react-icons/md";
 import { FiBarChart2, FiFileText, FiUsers } from "react-icons/fi";
 import { IoSettingsOutline } from "react-icons/io5";
 
-export const dshboardList = [
+export const mobileNavbarList = [
   {
     id: 1,
-    icon: <MdDashboard size={18} />,
+    icon: <MdDashboard size={17} />,
     name: "Dashboard",
     link: "/",
   },
   {
     id: 2,
-    icon: <FiUsers size={18} />,
+    icon: <FiUsers size={17} />,
     name: "All classes",
     link: "/",
   },
   {
     id: 3,
-    icon: <FiFileText size={18} />,
+    icon: <FiFileText size={17} />,
     name: "Assignments",
     link: "/",
   },
   {
     id: 4,
-    icon: <FiBarChart2 size={18} />,
+    icon: <FiBarChart2 size={17} />,
     name: "Performance",
     link: "/",
   },
   {
     id: 5,
-    icon: <MdOutlineCurrencyRupee size={18} />,
+    icon: <MdOutlineCurrencyRupee size={17} />,
     name: "Fee details",
     link: "/",
   },
   {
     id: 6,
-    icon: <IoSettingsOutline size={18} />,
+    icon: <IoSettingsOutline size={17} />,
     name: "Settings",
     link: "/",
   },
